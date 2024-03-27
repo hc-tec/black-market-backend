9b38e33fbbbb8e69d1e87ec8f54d98ae56632e7c
qq_45663002 <2598772546@qq.com>
2024-03-27 23:39:14 +0800

    删除百度云API

--- SecondaryMarket/settings.py
+++ SecondaryMarket/settings.py
@@ -249,9 +249,3 @@ django.setup()
 
 
 
-# 通过环境变量传递（作用于全局，优先级最低）
-os.environ["QIANFAN_ACCESS_KEY"] = "ALTAKYXfMqD73nK78A3BjH1f3I"
-os.environ["QIANFAN_SECRET_KEY"] = "128f38da2fce4a4a8bbfab806a253d98"
-
-os.environ["QIANFAN_AK"] = "meF3YT1H8JfIkpkz7V6daAjz"
-os.environ["QIANFAN_SK"] = "CIypbmJoB7SxuNDo8w8J6k7xg40BooQL"
